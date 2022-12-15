ac73cd8dae003f5de859e5408aa07ea83c904a30
Prico3 <pricoco.andrea@gmail.com>
2022-12-15 16:43:21 +0100

    first commit

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,22 @@
+<header>
+    <div class="container d-flex">
+        <div class="logo">
+            <img src="{{ Vite::asset('resources/img/dc-logo.png') }}" alt="">
+        </div>
+        <nav class="navbar bg-light">
+            <div class="container-fluid d-flex">
+                <a class="navbar-brand" href="#">Characters</a>
+                <a class="navbar-brand" href="#">Comics</a>
+                <a class="navbar-brand" href="#">Movies</a>
+                <a class="navbar-brand" href="#">TV</a>
+                <a class="navbar-brand" href="#">Games</a>
+                <a class="navbar-brand" href="#">Collectibles</a>
+                <a class="navbar-brand" href="#">Videos</a>
+                <a class="navbar-brand" href="#">Fans</a>
+                <a class="navbar-brand" href="#">News</a>
+                <a class="navbar-brand" href="#">Shop</a>
+
+            </div>
+        </nav>
+    </div>
+</header>
